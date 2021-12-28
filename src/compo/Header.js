bc60826c772048579023aa05943c1598b32de70f
barbaro <48423441+barbaroreyes@users.noreply.github.com>
2021-12-27 21:55:50 -0600

    update

--- src/compo/Header.js
+++ src/compo/Header.js
@@ -1,21 +1,40 @@
 import React from 'react' ;
 import Navigation from './Navigation'
+import ImageP from '../images/foto-perfil.jpeg'
 import style from 'styled-components';
 
 const Container  =  style.div`
 border : 1px solid black;
 display:flex;
-justify-content:space-around;
+justify-content:space-between;
 align-items:center;
 height:50px;
+padding:20px;
 
 `
+const ImageContainer= style.div`
+width : 20%;
+height:50px;
+`
+const Profile = style.div`
+display: flex;
 
+align-items:center
+`
+const Name = style.h3`
+`
+const Image = style.img`
+width:59%;
+`
 
 const Header = () => {
   return (
     <Container>
-     <div>Barbaro Reyes</div>
+     <Profile>
+     <ImageContainer>
+    <Image src ={ImageP} alt="" />
+    </ImageContainer><Name>Barbaro Reyes</Name>
+    </Profile>
      <Navigation/>
     </Container>
   )
