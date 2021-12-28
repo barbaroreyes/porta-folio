53deef96bfa725a8e0b7e1a0e89132175379a19a
barbaro <48423441+barbaroreyes@users.noreply.github.com>
2021-12-27 22:26:51 -0600

    update

--- src/compo/Header.js
+++ src/compo/Header.js
@@ -6,25 +6,31 @@ import style from 'styled-components';
 const Container  =  style.div`
 border : 1px solid black;
 display:flex;
-justify-content:space-between;
+justify-content:space-around;
 align-items:center;
-height:50px;
-padding:20px;
+height:130px;
+
 
 `
 const ImageContainer= style.div`
 width : 20%;
-height:50px;
+border-radius : 50%;
+margin-left:20px;
 `
 const Profile = style.div`
 display: flex;
-
-align-items:center
+align-items:center;
+width:70%;
 `
-const Name = style.h3`
+const Name = style.p`
+display: flex;
+font-size : 25px;
+margin-left:0px;
 `
 const Image = style.img`
-width:59%;
+width:40%;
+border-radius : 50%;
+
 `
 
 const Header = () => {
