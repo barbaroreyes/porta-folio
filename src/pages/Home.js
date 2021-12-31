4e619e1b9ebf6249803475e57169603baf83dc4d
barbaro <48423441+barbaroreyes@users.noreply.github.com>
2021-12-30 22:18:10 -0600

    update

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -5,8 +5,8 @@ import {mobile} from '../compo/Responsive';
 
 const Container  =  style.div`
 display:flex;
-height:120vh;
-${mobile({height:'88vh'})}
+height:80vh;
+${mobile({height:'89vh'})}
 
 `
 const Left = style.div`
@@ -14,7 +14,8 @@ display:flex;
 justify-content:center;
 flex-direction:row;
 align-items:center;
-width:100%;background:rgb(231,221,200);
+width:100%;
+background:rgb(231,221,200);
 ${mobile({
   flexDirection:'column',
   margin:'1px'
@@ -32,34 +33,35 @@ background:rgb(231,221,200);
 const BarSkill = style.div`
 text-align:center;
 display:flex;
+flex-wrap:wrap;
 justify-content:center;
 align-item:center;
-width:80%;
 margin:2px;
 height:60vh;
 ${mobile({
   width:'80%',
-  padding:'7px'
+  height:'75vh'
 })}
 `
-const H2 = style.h4`
-font-size: 28px;
-color:#864141;
-font-family: 'Island Moments', cursive;
-${mobile({
-  width:'50%',
-  padding:'1px',
-  fontSize:'45px'
-})}
-`
-const Text= style.p`
+
+const Text= style.span`
+display:flex;
+justify-content:center;
+align-items:center;
 font-size: 15px;
 padding:3px;
 border-radius:10px 10px;
 background:#864141;
 color:#ffff;
 font-family: 'Island Moments', cursive;
-
+${mobile({
+  width:'30%',
+  margin:'25px',
+  fontSize:'39px',
+  background:'none',
+  color:'#864141',
+  fontWeight:'bold'
+})}
 `
 
 const Home = () => {
@@ -67,20 +69,19 @@ const Home = () => {
     <Container>
       <Left>
         <BarSkill>
-           <H2>
-           Technologies
             <Text>React</Text>
             <Text>JavaScript</Text>
             <Text>Typescript</Text>
+            <Right>
+          <Slide/>
+          </Right>
             <Text>NextJS</Text>
             <Text>Mongo DB</Text>
             <Text>NodeJs</Text>
             <Text>AWS</Text>
-          </H2>
+  
          </BarSkill>
-         <Right>
-      <Slide/>
-      </Right>
+      
         {/* <BarSkill>
           <H2>Tools
           <Text>CSS3</Text>
