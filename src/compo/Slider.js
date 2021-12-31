ca2eddfd4819f248fdb4547501173e27926683b1
barbaro <48423441+barbaroreyes@users.noreply.github.com>
2021-12-31 00:49:31 -0600

    update

--- src/compo/Slider.js
+++ src/compo/Slider.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import style from 'styled-components';
+import {useNavigate} from 'react-router-dom'
 import 'react-responsive-carousel/lib/styles/carousel.min.css';
 import {Carousel} from 'react-responsive-carousel'
 import {mobile} from '../compo/Responsive'
@@ -14,10 +15,11 @@ ${mobile({width: '400px'})}
 `
 
 const Slider = () => {
+    const history = useNavigate()
   return (
     <Container>
         <Carousel infiniteLoop autoPlay>
-        <Image src='https://finalappchinchorro0029dd132ee54bd4b414de81bee3b82034-dev.s3.amazonaws.com/public/images/app-store-beers+2.png' alt='pict' />
+        <Image src='https://finalappchinchorro0029dd132ee54bd4b414de81bee3b82034-dev.s3.amazonaws.com/public/images/app-store-beers+2.png' onClick={()=> history('/portafolio')} alt='pict' />
         <Image src='https://finalappchinchorro0029dd132ee54bd4b414de81bee3b82034-dev.s3.amazonaws.com/public/images/elchinchorro-picture.png' alt='pict' />
         <Image src='https://finalappchinchorro0029dd132ee54bd4b414de81bee3b82034-dev.s3.amazonaws.com/public/images/pets-pictures.png' alt='pict' />
     <Image src='https://finalappchinchorro0029dd132ee54bd4b414de81bee3b82034-dev.s3.amazonaws.com/public/images/app-movie.png' alt='pict'/>
